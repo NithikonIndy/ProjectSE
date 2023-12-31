dfe2b801840269b17d7508ff8e2109020ee55da0
jirayu <jirayu004za@gmail.com>
2023-12-31 14:30:19 +0700

    implement func getAuthenticatedAdmin to working

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -24,7 +24,30 @@ const getAuthenticatedUser = asyncHandler(async (req, res, next) => {
   }catch(error){
     return next(error);
   }
-
 });
 
-export { getAuthenticatedUser };
+
+const getAuthenticatedAdmin = asyncHandler(async (req, res, next) => {
+  const authenticatedUser = await req.session.userId;
+  // log authenticated user id in session
+  console.log(`Authenticated userId in session: ${authenticatedUser}`);
+
+  const authenticatedAdmin  = await User.findById(authenticatedUser);
+
+  try{
+    if(!authenticatedAdmin || authenticatedAdmin === undefined || authenticatedAdmin === null || authenticatedAdmin.role !== 'ADMIN'){
+      // log if user is not authenticatedAdmin
+      //(`${authenticatedAdmin.name} is not authenticated role to be admin`);
+      res.redirect("/home");
+    }else{
+      // log check for admin role in db
+      console.log(`${authenticatedAdmin.name} authenticated role to be ${authenticatedAdmin.role}`);
+      res.status(200);
+      next();
+    }
+  }catch(error){
+    return next(res.redirect("/signIn"));
+  }
+});
+
+export { getAuthenticatedUser, getAuthenticatedAdmin};

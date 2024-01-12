c035ad8f632c6cd7647a9f87cb1848365685e804
jirayu <jirayu004za@gmail.com>
2024-01-12 16:08:43 +0700

    add enum reasons an export

--- Backend/utils/constants.js
+++ Backend/utils/constants.js
@@ -4,4 +4,9 @@ const roles = {
     client: 'CLIENT',
 }
 
-export default { roles }
+const reasons = {
+    reasons_one: 'reasons_one',
+    reasons_two: 'reasons_two',
+    reasons_three: 'reasons_three',
+}
+export default { roles, reasons }

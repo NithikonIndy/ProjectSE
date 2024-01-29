a39342c53aa67eece240806b2d3a39b57d5553b7
jirayu <jirayu004za@gmail.com>
2024-01-30 00:08:08 +0700

    .

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import CommentPage from './CommentPage';
-import ReportPage from './ReportPage';
-=======
 import CommentPage from './CommentPage/CommentPage';
 import ReportPage from './ReportPage/ReportPage';
 import LoginPage from "./LoginPage/LoginPage";
 import HomePage from './Homepage/Homepage';
->>>>>>> f4bb12d4010dc816ee48dc8fa8d2776a4715669b
 import './App.css';
 
 function App() {
@@ -17,11 +12,8 @@ function App() {
       <Routes>
         <Route path="/post" element={<CommentPage />} />
         <Route path="/report" element={<ReportPage />} />
-<<<<<<< HEAD
-=======
         <Route path="/login" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
->>>>>>> f4bb12d4010dc816ee48dc8fa8d2776a4715669b
       </Routes>
     </Router>
   );
---
+++ Frontend/src/LoginPage/LoginPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./Header.js";
+import "./LoginPage.css"; // Make sure to create this CSS file with the styles provided below
+import { Link } from "react-router-dom";
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <Header />
+
+      <div className="login-content">
+        <h1>CMU FreeBird</h1>
+        <p>Anonymous Social For CPE</p>
+        <Link to="http://localhost:3000/signIn">
+          <button>Sign In With CMU Account</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;

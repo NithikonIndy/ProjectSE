f1cc1221927d7e38b65111b99398808a8eb01066
jirayu <jirayu004za@gmail.com>
2024-01-30 00:26:00 +0700

    del

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,18 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createTheme, MantineProvider } from '@mantine/core';
-const theme = createTheme({
-  /** Put your mantine theme override here */
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <MantineProvider>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </MantineProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function

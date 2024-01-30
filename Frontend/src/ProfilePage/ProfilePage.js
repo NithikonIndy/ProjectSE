c683f308441f838ddf00840972543093f307a6b7
jirayu <jirayu004za@gmail.com>
2024-01-31 01:07:01 +0700

    add template profile

---
+++ Frontend/src/ProfilePage/ProfilePage.js
@@ -0,0 +1,12 @@
+import Header from "../Header/Header.js";
+import "./ProfilePage.css"
+import React from "react";
+
+const ProfilePage = () => {
+  return (
+    <div className="profile-page">
+      <Header />
+    </div>
+  );
+};
+export default ProfilePage;

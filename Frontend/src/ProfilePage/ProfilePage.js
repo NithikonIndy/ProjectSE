bad9cf98901660099811a3885e84ee7eea860083
jirayu <jirayu004za@gmail.com>
2024-01-31 01:16:20 +0700

    update profile page

--- Frontend/src/ProfilePage/ProfilePage.js
+++ Frontend/src/ProfilePage/ProfilePage.js
@@ -1,3 +1,4 @@
+import { Container } from "react-bootstrap";
 import Header from "../Header/Header.js";
 import "./ProfilePage.css"
 import React from "react";
@@ -6,6 +7,9 @@ const ProfilePage = () => {
   return (
     <div className="profile-page">
       <Header />
+      <Container className="container-profile-page">
+        profilePage
+      </Container>
     </div>
   );
 };

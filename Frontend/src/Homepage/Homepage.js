0b9cdd95f488bb67ec17391fa887fd193d2ff38d
jirayu <jirayu004za@gmail.com>
2024-02-02 19:11:26 +0700

    update with container

--- Frontend/src/Homepage/Homepage.js
+++ Frontend/src/Homepage/Homepage.js
@@ -1,28 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Header from '../Header/Header';
-import './Homepage.css';
-import ReportPage from '../ReportPage/ReportPage';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThumbsUp, faFlag, faEdit } from '@fortawesome/free-solid-svg-icons';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import { uniqueNamesGenerator, Config, animals } from 'unique-names-generator';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "../Header/Header";
+import "./Homepage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp, faFlag, faEdit } from "@fortawesome/free-solid-svg-icons";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { uniqueNamesGenerator, Config, animals } from "unique-names-generator";
 import Swal from "sweetalert2";
+import { Container } from "react-bootstrap";
 
 const MessageContainer = ({ message }) => (
-  <div className="message-container">
-    {message}
-  </div>
-);
-
-const ReportPopup = ({ onClose, onReport }) => (
-  <div className="popup">
-    <div className="popup-inner">
-      <button onClick={onReport}>Report</button>
-      <button onClick={onClose}>Cancel</button>
-    </div>
-  </div>
+  <div className="message-container">{message}</div>
 );
 
 const generateRandomNameForUserId = (userId) => {
@@ -35,16 +24,12 @@ const generateRandomNameForUserId = (userId) => {
   return uniqueNamesGenerator(config);
 };
 
-
-
 const Homepage = () => {
-  const [blogText, setBlogText] = useState('');
+  const [blogText, setBlogText] = useState("");
   const [blogs, setBlogs] = useState([]);
   const [blogLikes, setBlogLikes] = useState({});
-  const [postMessage, setPostMessage] = useState('');
+  const [postMessage, setPostMessage] = useState("");
   const navigate = useNavigate();
-  const [blogToReport, setBlogToReport] = useState(null);
-  const [showReportPopup, setShowReportPopup] = useState(false);
   const [editMode, setEditMode] = useState(false);
   const [editedBlogId, setEditedBlogId] = useState(null);
   const [users, setUsers] = useState([]);
@@ -52,7 +37,6 @@ const Homepage = () => {
   const [likespost, setLikespost] = useState([]);
   const [clickedBlogId, setClickedBlogId] = useState([]);
   const [blogdelete, setblogdelete] = useState([]);
-
 
   const AlertDelete = (blogid) => {
     Swal.fire({
@@ -74,11 +58,13 @@ const Homepage = () => {
     });
   };
 
-  const AlertReport = async(blogid) => {
+  const AlertReport = async (blogid) => {
     console.log(blogid);
     try {
       // deconstruct the response to get the data
-      const { data : fetchReasons } = await axios.get("http://localhost:3000/reportReasons");
+      const { data: fetchReasons } = await axios.get(
+        "http://localhost:3000/reportReasons"
+      );
       console.log(fetchReasons);
 
       Swal.fire({
@@ -97,7 +83,7 @@ const Homepage = () => {
             inputPlaceholder: "Please select your reasons",
             showCancelButton: true,
             inputValidator: (result) => {
-              console.log("inputValidator:" ,result);
+              console.log("inputValidator:", result);
               return !result && "You need to select the reason!";
             },
           });
@@ -105,7 +91,11 @@ const Homepage = () => {
           if (reasons) {
             try {
               const reason = fetchReasons[reasons];
-              await axios.post(`http://localhost:3000/api/blog/${blogid}/report`, {reason} , {withCredentials: true});
+              await axios.post(
+                `http://localhost:3000/api/blog/${blogid}/report`,
+                { reason },
+                { withCredentials: true }
+              );
             } catch (error) {
               console.log(error);
             }
@@ -122,21 +112,20 @@ const Homepage = () => {
     }
   };
 
-
   useEffect(() => {
     const getSession = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/Userid', { withCredentials: true });
+        const response = await axios.get("http://localhost:3000/Userid", {
+          withCredentials: true,
+        });
         setUsers([response.data.user]);
       } catch (error) {
-        console.error('Error fetching user session:', error);
+        console.error("Error fetching user session:", error);
       }
     };
 
     getSession();
   }, []);
-
-
 
   const onClickgetblogId = async (blogId) => {
     setClickedBlogId(blogId);
@@ -151,27 +140,23 @@ const Homepage = () => {
     }, 250);
   };
 
-
   const handleBlogChange = (event) => {
     setBlogText(event.target.value);
   };
 
-
   const handlePostBlog = async () => {
-    await axios.post(
-      `http://localhost:3000/api/blog/add`,
-      {
+    await axios
+      .post(`http://localhost:3000/api/blog/add`, {
         user: users[0], // Assuming users is an array and you want the first user
         description: blogText,
-      }
-    ).then((response) => {
-      SetBlogs([...Blogs, response.data.blog]);
-      setBlogText('');
-    })
+      })
+      .then((response) => {
+        SetBlogs([...Blogs, response.data.blog]);
+        setBlogText("");
+      })
       .catch((error) => {
         console.error("Error adding comment:", error);
       });
-
   };
 
   const handleLikeBlog = async (blogId) => {
@@ -193,27 +178,10 @@ const Homepage = () => {
     } else {
       console.log("No blogId available");
     }
-
-  };
-
-  const handleReportBlog = (blog) => {
-    setBlogToReport(blog);
-    setShowReportPopup(true);
-  };
-
-  const closeReportPopup = () => {
-    setBlogToReport(null);
-    setShowReportPopup(false);
-  };
-
-  const goToReportPage = () => {
-    // ใช้ navigate เพื่อไปที่หน้า ReportPage และส่งข้อมูล blog ที่ต้องการรายงานไปด้วย
-    navigate(`/report/`);
-    closeReportPopup(); // ปิด pop-up เมื่อไปที่หน้า ReportPage
   };
 
   const handleEditBlog = (blogId) => {
-    const blogToEdit = blogs.find(blog => blog.id === blogId);
+    const blogToEdit = blogs.find((blog) => blog.id === blogId);
     if (blogToEdit) {
       setBlogText(blogToEdit.content);
       setEditMode(true);
@@ -222,21 +190,23 @@ const Homepage = () => {
   };
 
   const handleSaveEdit = () => {
-    if (blogText.trim() !== '') {
-      setBlogs(prevBlogs => prevBlogs.map(blog =>
-        blog.id === editedBlogId ? { ...blog, content: blogText } : blog
-      ));
-      setBlogText('');
-      setPostMessage('Blog edited successfully!');
+    if (blogText.trim() !== "") {
+      setBlogs((prevBlogs) =>
+        prevBlogs.map((blog) =>
+          blog.id === editedBlogId ? { ...blog, content: blogText } : blog
+        )
+      );
+      setBlogText("");
+      setPostMessage("Blog edited successfully!");
       setEditMode(false);
       setEditedBlogId(null);
     } else {
-      setPostMessage('Please enter a blog before editing.');
+      setPostMessage("Please enter a blog before editing.");
     }
   };
 
   const handleCancelEdit = () => {
-    setBlogText('');
+    setBlogText("");
     setEditMode(false);
     setEditedBlogId(null);
   };
@@ -244,50 +214,42 @@ const Homepage = () => {
   const hidedeletebuttons = (blogId) => {
     const isBlogOwner = blogId === users[0];
     const buttonToToggle = document.getElementById(`deleteButton-${blogId}`);
-    
+
     if (buttonToToggle) {
       buttonToToggle.style.display = isBlogOwner ? "block" : "none";
     }
   };
 
-
   const handleDeleteBlog = (blogId) => {
-    const apiurl = `http://localhost:3000/api/blog/${blogId}`
-    axios.delete(apiurl)
-      .then(response => {
-        console.log('Delete successful', response.data);
+    const apiurl = `http://localhost:3000/api/blog/${blogId}`;
+    axios
+      .delete(apiurl)
+      .then((response) => {
+        console.log("Delete successful", response.data);
       })
-      .catch(error => {
-        console.error('Error deleting resource', error);
+      .catch((error) => {
+        console.error("Error deleting resource", error);
       });
     console.log("hello", blogId);
   };
 
   const fetchBlogs = async () => {
-    ;
     try {
-      const response = await axios.get('http://localhost:3000/api/blog');
+      const response = await axios.get("http://localhost:3000/api/blog");
       let i = 0;
       let end = response.data.blogs.length - 1;
       const Blog = [];
       while (end >= i) {
-
         Blog.push(response.data.blogs[end]);
         end--;
       }
       SetBlogs(Blog);
 
       // setLikespost([response.data.blog.likes.length]);
-
-
     } catch (error) {
-      console.error('Error fetching blogs:', error);
+      console.error("Error fetching blogs:", error);
     }
-
-
-
   };
-
 
   useEffect(() => {
     fetchBlogs();
@@ -297,36 +259,43 @@ const Homepage = () => {
     }, 100);
   }, []);
 
-
-
   return (
     <div className="homepage">
       <Header />
-      <h4>Blogs</h4>
-      <div className="blog-section">
-        <textarea
-          placeholder="Write your blog here..."
-          value={blogText}
-          onChange={handleBlogChange}
-        />
-        <button onClick={() => {
-          handlePostBlog();
-          setTimeout(() => {
-            fetchBlogs();
-          }, 350);
-        }}>
-          Post Blog</button>
-      </div>
 
-      {/* {blog} */}
-      <div className="existing-blogs">
+      <Container className="padding-container">
+        <h4>Blogs</h4>
+        <div className="blog-section">
+          <textarea
+            placeholder="Write your blog here..."
+            value={blogText}
+            onChange={handleBlogChange}
+          />
+          <button
+            onClick={() => {
+              handlePostBlog();
+              setTimeout(() => {
+                fetchBlogs();
+              }, 350);
+            }}
+          >
+            Post Blog
+          </button>
+        </div>
+
+        {/* {blog} */}
         {Array.isArray(Blogs) &&
           Blogs.map((blog) => (
-            <div key={blog._id} className="blog-item" >
-              <div onClick={() => { onClickgetblogId(blog._id); }}>
+            <div key={blog._id} className="blog-item">
+              <div
+                onClick={() => {
+                  onClickgetblogId(blog._id);
+                }}
+              >
                 <p>{generateRandomNameForUserId(blog.user)}</p>
                 <p>{blog.description}</p>
               </div>
+
               <div className="blog-icons">
                 <button onClick={() => onClicklikeblog(blog._id)}>
                   <FontAwesomeIcon icon={faThumbsUp} />
@@ -338,7 +307,6 @@ const Homepage = () => {
                 <button onClick={() => handleEditBlog(blog.id)}>
                   <FontAwesomeIcon icon={faEdit} />
                 </button>
-
 
                 {blog.user === users[0] && (
                   <button
@@ -353,56 +321,23 @@ const Homepage = () => {
                     Delete
                   </button>
                 )}
-
               </div>
-
+              
             </div>
           ))}
-      </div>
 
-
-
-
-      <div className="existing-blogs">
-        {blogs.map((blog) => (
-          <div key={blog.id} className="blog-item">
-            <MessageContainer message={blog.content} />
-            <div className="blog-icons">
-              <button onClick={() => handleLikeBlog(blog.id)}>
-                <FontAwesomeIcon icon={faThumbsUp} />
-              </button>
-              {blogLikes[blog.id] > 0 && <span>{blogLikes[blog.id]} ❤️</span>}
-              <button onClick={() => handleReportBlog(blog)}>
-                <FontAwesomeIcon icon={faFlag} />
-              </button>
-              <button onClick={() => handleEditBlog(blog.id)}>
-                <FontAwesomeIcon icon={faEdit} />
-              </button>
-              <button onClick={() => handleDeleteBlog(blog.id)}>
-                Delete
-              </button>
-
-            </div>
+        {editMode && (
+          <div>
+            <textarea
+              placeholder="Edit your blog here..."
+              value={blogText}
+              onChange={(e) => setBlogText(e.target.value)}
+            />
+            <button onClick={handleSaveEdit}>Save Edit</button>
+            <button onClick={handleCancelEdit}>Cancel Edit</button>
           </div>
-        ))}
-      </div>
-
-    
-      {showReportPopup && (
-        <ReportPopup onClose={closeReportPopup} onReport={goToReportPage} />
-      )}
-      {editMode && (
-        <div>
-          <textarea
-            placeholder="Edit your blog here..."
-            value={blogText}
-            onChange={(e) => setBlogText(e.target.value)}
-          />
-          <button onClick={handleSaveEdit}>Save Edit</button>
-          <button onClick={handleCancelEdit}>Cancel Edit</button>
-        </div>
-      )}
-      {/* ... (ต่อไป) */}
+        )}
+      </Container>
     </div>
   );
 };

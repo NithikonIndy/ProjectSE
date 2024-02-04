6ec937590db22a10ba2398895aadbd3adeb68901
jirayu <jirayu004za@gmail.com>
2024-02-04 07:08:43 +0700

    clean code v1

--- Frontend/src/CommentPage/CommentPage.js
+++ Frontend/src/CommentPage/CommentPage.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import "./CommentPage.css";
 import "../components/comment/comment.css";
 import Header from "../Header/Header.js";
-import { uniqueNamesGenerator, Config, animals } from "unique-names-generator";
-import { set } from "mongoose";
-import Blog from "../components/blogs/Blog.js";
-import Comment from "../components/comment/Comment.js";
-import CommentInput from "../components/comment/CommentInput.js";
+import "../components/blogs/blog.css"
+import "../components/comment/comment.css";
+import "../components/comment/commentInput.css";
+import { uniqueNamesGenerator, animals } from "unique-names-generator";
 import {
   Button,
   Card,
@@ -27,6 +25,7 @@ import {
   faEdit,
 } from "@fortawesome/free-solid-svg-icons";
 import Swal from "sweetalert2";
+import axios from "axios";
 
 const generateRandomNameForUserId = (userId, blogId) => {
   const seed = userId + blogId; // Use the user ID as the seed
@@ -40,176 +39,106 @@ const generateRandomNameForUserId = (userId, blogId) => {
 
 const CommentPage = () => {
   const [comments, setComments] = useState([]);
-  const [commentText, setCommentText] = useState([]);
+  //const [commentText, setCommentText] = useState([]);
   const [blogs, setBlogs] = useState([]);
   const [newComment, setNewComment] = useState("");
   const [users, setUsers] = useState([]);
   const [blogComment, setBlogComment] = useState([]);
-  const [clickedBlogId, setClickedBlogId] = useState([]);
+  //const [clickedBlogId, setClickedBlogId] = useState([]);
   const [likespost, setLikespost] = useState();
-  const [clickedcommentId, setClickedcommentId] = useState([]);
+  //const [clickedcommentId, setClickedcommentId] = useState([]);
   const blogIdforget = useParams().blogId;
   const [userRole, setUserRole] = useState([]);
-  const [blogText, setBlogText] = useState("");
+  //const [blogText, setBlogText] = useState("");
 
   useEffect(() => {
-    const fetchUserRole = async () => {
-      try {
-        const { data: role } = await axios.get(
-          "http://localhost:3000/session",
-          { withCredentials: true }
-        );
+    fetchBlogs();
+    fetchSession();
+    fetchUserRole();
+    fetchComments();
+
+    console.log("User role:", userRole,"\n",
+                "User session:", users[0] ,"\n",
+                "blog-Comment:", blogComment, "\n",
+                "User session:", users[0] ,"\n",
+    );
+  }, [userRole], [users], [blogComment], [users[0]]);    
+  
+  const fetchSession = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/Userid", {withCredentials: true,});
+      setUsers([response.data.user]);
+    } catch (error) {
+      console.error("Error fetching user session:", error);
+    }
+  };
+
+  const fetchUserRole = async () => {
+    try {
+      const { data: role } = await axios.get("http://localhost:3000/session",{ withCredentials: true });
         setUserRole(role);
         //console.log(role);
         //console.log(userRole);
       } catch (error) {
         console.error("Error fetching user role:", error);
       }
-    };
-    fetchUserRole();
-  }, [userRole]);
-
-  const AlertDelete = (blogid) => {
-    Swal.fire({
-      title: " blog Firmly to delete?",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire({
-          title: "Deleted!",
-          text: "Your file has been deleted.",
-          icon: "success",
-        });
-        handleDeleteBlog(blogid);
-      }
-    });
   };
 
-  const AlertDeleteComment = (commentid) => {
-    Swal.fire({
-      title: "commnet Firmly to delete?",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire({
-          title: "Deleted!",
-          text: "Your file has been deleted.",
-          icon: "success",
-        });
-        handleDeleteComment(commentid);
-      }
-    });
-  };
-
-  const AlertReport = async (blogid) => {
+  const fetchComments = async () => {
     try {
-      // deconstruct the response to get the data //* console.log(fetchReasons); *//
-      const { data: fetchReasons } = await axios.get(
-        "http://localhost:3000/reportReasons"
+      const response = await axios.get(
+        `http://localhost:3000/api/comments/blogs/${blogIdforget}`
       );
-      console.log(fetchReasons);
-      console.log(blogid);
+      let i = 0;
+      let end = response.data.blogcomments.length;
+      const accumulatedComments = [];
 
-      Swal.fire({
-        title: "Firmly to report?",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, report it!",
-      }).then(async (result) => {
-        if (result.isConfirmed) {
-          //console.log(blogs[0]._id);
-          //console.log(users[0]);
-          const { value: reasons } = await Swal.fire({
-            title: "Please select your reasons",
-            input: "select",
-            inputOptions: fetchReasons,
-            inputPlaceholder: "Please select your reasons",
-            showCancelButton: true,
-            inputValidator: (result) => {
-              console.log("inputValidator:", result);
-              return !result && "You need to select the reason!";
-            },
-          });
-          if (reasons) {
-            // fetch the POST reasons from the backend
-            console.log(`reasons[${reasons}]`);
-            try {
-              const reason = fetchReasons[reasons];
-              console.log(`reasons ${reason}`);
-              await axios.post(
-                `http://localhost:3000/api/blog/${blogid}/report`,
-                { reason },
-                { withCredentials: true }
-              );
-            } catch (error) {
-              console.error(error);
-            }
-            Swal.fire({
-              title: "Report!",
-              text: `Your report reason[${reasons}] has submitted.`,
-              icon: "success",
-            });
-          }
+      while (i < end) {
+        if (response.data.blogcomments[i].blog == blogComment) {
+          accumulatedComments.push(response.data.blogcomments[i]);
         }
-      });
+        i++;
+      }
+      //console.log(response.data);
+      setComments(accumulatedComments);
+    } catch (err) {
+      console.log("Error fetching comments:", err);
+    }
+  };
+
+  /*one blog */
+  const fetchBlogs = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/blog/${blogIdforget}`
+      );
+
+      setBlogs([response.data.blog]);
+      setBlogComment([response.data.blog._id]);
+      setLikespost([response.data.blog.likes.length]);
+    } catch (error) {
+      console.error("Error fetching blogs:", error);
+    }
+  };
+
+  const handleReportReasons = async() => {
+    try {
+      const { data: fetchReasons } = await axios.get("http://localhost:3000/reportReasons");
+      console.log(fetchReasons);
+      return fetchReasons;
     } catch (error) {
       console.error(error);
     }
   };
 
-  const AlertEdit = (blogid) => {
-    Swal.fire({
-      title: "Enter text",
-      input: "text",
-      inputLabel: "Your text",
-      showCancelButton: true,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        const editedText = result.value;
-        if (editedText) {
-          setBlogText(editedText); // Set the edited text to the state
-          Swal.fire({
-            title: "Edit!",
-            text: `Your post has been edited.`,
-            icon: "success",
-          });
-          handleEditBlog(blogid, editedText); // Pass the edited text to handleEditBlog
-        }
-      }
-    });
-  };
-
-  const AlertEditComment = (commentid) => {
-    Swal.fire({
-      title: "Enter text",
-      input: "text",
-      inputLabel: "Your text",
-      showCancelButton: true,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        const editedText = result.value;
-        if (editedText) {
-          setCommentText(editedText); // Set the edited text to the state
-          Swal.fire({
-            title: "Edit!",
-            text: `Your comment has been edited.`,
-            icon: "success",
-          });
-          console.log("edit comment text's: ",editedText);
-          handleEditComment(commentid, editedText); // Pass the edited text to handleEditBlog
-        }
-      }
-    });
+  const handleReport = async (blogid, selectReason) => {
+    const reason = selectReason;
+    console.log(`reason:`, reason);
+    try {
+      await axios.post(`http://localhost:3000/api/blog/${blogid}/report`, { reason }, { withCredentials: true });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const handleEditBlog = (blogId, editedText) => {
@@ -230,102 +159,6 @@ const CommentPage = () => {
     }, 400);
   };
 
-  const onClickgetblogId = async (blogId) => {
-    console.log("Clicked on blog with ID:", blogId);
-    setClickedBlogId(blogId); // Store the clicked blogId in state
-    likePost(blogId); // Call likePost with the blogId
-    setTimeout(() => {
-      fetchlike(blogId);
-    }, 250);
-  };
-
-  const onClickgetcommentId = async (commentId) => {
-    console.log("Clicked on Comment with ID:", commentId);
-    setClickedcommentId(commentId);
-    likecoment(commentId);
-    setTimeout(() => {
-      fetchComments();
-    }, 250);
-  };
-
-  const showdeletebuttons = (UserId) => {
-    const isBlogOwner = UserId === users[0];
-    const buttonToToggle = document.getElementById(`deleteButton-${UserId}`);
-
-    if (buttonToToggle) {
-      buttonToToggle.style.display = isBlogOwner ? "block" : "none";
-    }
-  };
-
-  const showdeletebuttonsforcomment = (UserId) => {
-    const isBlogOwner = UserId === users[0];
-    const buttonToToggle = document.getElementById(
-      `deleteButtoncomment-${UserId}`
-    );
-
-    if (buttonToToggle) {
-      buttonToToggle.style.display = isBlogOwner ? "block" : "none";
-    }
-  };
-
-  /*Userid */
-  useEffect(() => {
-    const getSession = async () => {
-      try {
-        const response = await axios.get("http://localhost:3000/Userid", {
-          withCredentials: true,
-        });
-        setUsers([response.data.user]);
-      } catch (error) {
-        console.error("Error fetching user session:", error);
-      }
-    };
-
-    getSession();
-  }, []);
-  /*one blog */
-  const fetchBlogs = async () => {
-    try {
-      const response = await axios.get(
-        `http://localhost:3000/api/blog/${blogIdforget}`
-      );
-
-      setBlogs([response.data.blog]);
-      setBlogComment([response.data.blog._id]);
-      setLikespost([response.data.blog.likes.length]);
-    } catch (error) {
-      console.error("Error fetching blogs:", error);
-    }
-  };
-
-  /*comment*/
-  useEffect(() => {
-    const fetchComments = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:3000/api/comments/blogs/${blogIdforget}`
-        );
-        let i = 0;
-        let end = response.data.blogcomments.length;
-        const accumulatedComments = [];
-
-        while (i < end) {
-          if (response.data.blogcomments[i].blog == blogComment) {
-            accumulatedComments.push(response.data.blogcomments[i]);
-          }
-          i++;
-        }
-        //console.log(response.data);
-        setComments(accumulatedComments);
-      } catch (err) {
-        console.log("Error fetching comments:", err);
-      }
-    };
-
-    fetchComments();
-  }, [blogComment]);
-
-  /*addcomment */
   const handleAddComment = () => {
     axios
       .post(`http://localhost:3000/api/comments/blog/${blogIdforget}/add`, {
@@ -427,7 +260,7 @@ const CommentPage = () => {
       })
       .then((response) => {
         setComments(...comments, response.data.comment);
-        setCommentText("");
+        //setCommentText("");
         console.log("Edit successful", response.data);
       })
       .catch((error) => {
@@ -437,34 +270,149 @@ const CommentPage = () => {
       fetchComments();
     }, 400);
   };
-
-  const fetchComments = async () => {
-    try {
-      const response = await axios.get(
-        `http://localhost:3000/api/comments/blogs/659f854a2ad66c8e8baf64f0`
-      );
-      let i = 0;
-      let end = response.data.blogcomments.length;
-      const accumulatedComments = [];
-
-      while (i < end) {
-        if (response.data.blogcomments[i].blog == blogComment) {
-          accumulatedComments.push(response.data.blogcomments[i]);
-        }
-        i++;
+  
+  const AlertDelete = (blogid) => {
+    Swal.fire({
+      title: " blog Firmly to delete?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+        handleDeleteBlog(blogid);
       }
-
-      setComments(accumulatedComments);
-    } catch (err) {
-      console.log("Error fetching comments:", err);
-    }
+    });
   };
 
-  useEffect(() => {
-    fetchBlogs();
-    showdeletebuttons(users[0]);
-    showdeletebuttonsforcomment(users[0]);
-  }, [users[0]]);
+  const AlertDeleteComment = (commentid) => {
+    Swal.fire({
+      title: "commnet Firmly to delete?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+        handleDeleteComment(commentid);
+      }
+    });
+  };
+
+  const AlertReport = async (blogid) => {
+      const fetchReasons = await handleReportReasons();
+      console.log(blogid);
+
+      Swal.fire({
+        title: "Firmly to report?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, report it!",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          //console.log(blogs[0]._id);
+          //console.log(users[0]);
+          const { value: reasons } = await Swal.fire({
+            title: "Please select your reasons",
+            input: "select",
+            inputOptions: fetchReasons,
+            inputPlaceholder: "Please select your reasons",
+            showCancelButton: true,
+            inputValidator: (result) => {
+              console.log("inputValidator:", result);
+              return !result && "You need to select the reason!";
+            },
+          });
+          if (reasons) {
+            // fetch the POST reasons from the backend
+            console.log(`reasons[${reasons}]`);
+            await handleReport(blogid, fetchReasons[reasons])
+            Swal.fire({
+              title: "Report!",
+              text: `Your report reason[${reasons}] has submitted.`,
+              icon: "success",
+            });
+          }
+        }
+      });
+  };
+
+  const AlertEdit = (blogid) => {
+    Swal.fire({
+      title: "Enter text",
+      input: "text",
+      inputLabel: "Your text",
+      showCancelButton: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const editedText = result.value;
+        if (editedText) {
+          //setBlogText(editedText); // Set the edited text to the state
+          Swal.fire({
+            title: "Edit!",
+            text: `Your post has been edited.`,
+            icon: "success",
+          });
+          handleEditBlog(blogid, editedText); // Pass the edited text to handleEditBlog
+        }
+      }
+    });
+  };
+
+  const AlertEditComment = (commentid) => {
+    Swal.fire({
+      title: "Enter text",
+      input: "text",
+      inputLabel: "Your text",
+      showCancelButton: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const editedText = result.value;
+        if (editedText) {
+          //setCommentText(editedText); // Set the edited text to the state
+          Swal.fire({
+            title: "Edit!",
+            text: `Your comment has been edited.`,
+            icon: "success",
+          });
+          console.log("edit comment text's: ",editedText);
+          handleEditComment(commentid, editedText); // Pass the edited text to handleEditBlog
+        }
+      }
+    });
+  };
+
+  const onClickgetblogId = async (blogId) => {
+    console.log("Clicked on blog with ID:", blogId);
+    //setClickedBlogId(blogId); // Store the clicked blogId in state
+    likePost(blogId); // Call likePost with the blogId
+    setTimeout(() => {
+      fetchlike(blogId);
+    }, 250);
+  };
+
+  const onClickgetcommentId = async (commentId) => {
+    console.log("Clicked on Comment with ID:", commentId);
+    //setClickedcommentId(commentId);
+    likecoment(commentId);
+    setTimeout(() => {
+      fetchComments();
+    }, 250);
+  };
 
   return (
     <div className="comment-page">

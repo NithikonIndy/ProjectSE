dbfb8e36ef1f0b25ed64d89cb33ee6d1ec5d2a12
jirayu <jirayu004za@gmail.com>
2024-02-18 15:11:31 +0700

    add del report schema if blog id it same on report field postId

--- Backend/controllers/blog-controller.js
+++ Backend/controllers/blog-controller.js
@@ -107,15 +107,16 @@ export const deleteBlog = async (req , res, next) => {
 
     let blog;
     try{
-        blog =await Blog.findByIdAndDelete(id).populate("user").populate("comments");
+        blog = await Blog.findByIdAndDelete(id).populate("user").populate("comments");
         console.log(blog);
+
         if (!blog) {
             return res.status(404).json({ message: "Blog not found" });
         }
         await Comment.deleteMany({ _id: { $in: blog.comments.map(comment => comment._id) } });
-        
         await blog.user.blogs.pull(blog);
         await blog.user.updateOne({ $pull: { comments: { $in: blog.comments.map(comment => comment._id) } } });
+        await Report.deleteMany({ postId: id });
         await blog.user.save();
     }catch(err){
         return console.log(err);

72d90316987a6576e238c7a99a7118f6e982b896
jirayu <jirayu004za@gmail.com>
2024-02-22 20:39:58 +0700

    getOAuthSessions test fix

--- Backend/controllers/OAuthController.js
+++ Backend/controllers/OAuthController.js
@@ -7,7 +7,7 @@ import User from "../models/userModel.js";
 
 const getOAuthSessions = asyncHandler((async(req, res, next) => {
   try {
-    const user = req.session.userId;
+    const user = req.sessionID.findOne({userId: req.session.userId});
     console.log("req.session: ",req.session);
     console.log("User ID from session:", user);
     res.status(200).json({user});

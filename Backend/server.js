8ccec5f9c6d39a184ef2b40b78d1608aad2f79bf
jirayu <jirayu004za@gmail.com>
2024-02-23 18:36:29 +0700

    saveUninitialized: false

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
       path: '/',
       httpOnly: false,

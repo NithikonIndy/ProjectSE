5ba90bef0cd9d8dc8d926c26da6e19f77c9f3c58
youknowkeshi <76035490+youknowkeshi@users.noreply.github.com>
2024-02-24 20:17:14 +0700

    add row and col in header.js change design in Profile page

--- Frontend/src/Header/Header.js
+++ Frontend/src/Header/Header.js
@@ -3,41 +3,40 @@ import "./Header.css";
 import freebird from "./picture/freebird.png";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBell,
-  faHome,
-  faRightFromBracket,
-  faUser,
-} from "@fortawesome/free-solid-svg-icons";
+import { faBell, faHome, faRightFromBracket, faUser } from "@fortawesome/free-solid-svg-icons";
+import { Col, Row } from "antd";
 
 const Header = () => (
   <div className="header">
-    <div className="container-logo">
-      <span className="free">Free</span>
-      <span className="bird">Bird</span>
-    </div>
-
-    <div className="container-search">
-      <input type="text" placeholder="Search..." />
-    </div>
-
-    <div className="container-icons">
-      <Link to="/home">
-        <FontAwesomeIcon icon={faBell} alt="notify-icon" />
-      </Link>
-
-      <Link to="/home">
-        <FontAwesomeIcon icon={faHome} alt="home-icon" />
-      </Link>
-
-      <Link to="/profile">
-        <FontAwesomeIcon icon={faUser} alt="profile-icon" />
-      </Link>
-
-      <Link to="http://localhost:3000/logout">
-        <FontAwesomeIcon icon={faRightFromBracket} alt="logout-icon" />
-      </Link>
-    </div>
+    <Row justify="center" wrap>
+      <Col span={8} align="left">
+        <div className="container-logo" style={{margin:"8px"}}>
+          <span className="free">Free</span>
+          <span className="bird">Bird</span>
+        </div>
+      </Col>
+      <Col span={8} align="center">
+        <div className="container-search" style={{margin:"8px"}}>
+          <input type="text" placeholder="Search..." />
+        </div>
+      </Col>
+      <Col span={8} align="right">
+        <div className="container-icons">
+          <Link to="/home">
+            <FontAwesomeIcon icon={faBell} alt="notify-icon" style={{margin:"8px"}} />
+          </Link>
+          <Link to="/home">
+            <FontAwesomeIcon icon={faHome} alt="home-icon" style={{margin:"8px"}} />
+          </Link>
+          <Link to="/profile">
+            <FontAwesomeIcon icon={faUser} alt="profile-icon" style={{margin:"8px"}}/>
+          </Link>
+          <Link to="http://localhost:3000/logout">
+            <FontAwesomeIcon icon={faRightFromBracket} alt="logout-icon" style={{margin:"8px"}}/>
+          </Link>
+        </div>
+      </Col>
+    </Row>
   </div>
 );
 
--- Frontend/src/ProfilePage/ProfilePage.js
+++ Frontend/src/ProfilePage/ProfilePage.js
@@ -1,62 +1,54 @@
 import React from "react";
 import Header from "../Header/Header.js";
 import "./ProfilePage.css";
-import { Card, Avatar } from 'antd';
+import { Card, Avatar, Divider, Typography, Button } from 'antd';
 import { Col, Row } from 'antd';
 import { Link } from "react-router-dom";
 
-const { Meta } = Card;
-
-const gridStyle = {
-  width: '25%',
-  textAlign: 'center',
-};
+const { Title, Text } = Typography;
 
 const ProfilePage = () => {
   return (
     <div>
       <Header />
-      <Row justify="left" style={{marginTop:"80px"}}>
+      <Row justify="center" style={{ marginTop: "80px" }}>
         <Col span={8} align="middle">
           <Card
             style={{
-              width: 500,
+              width: "100%",
+              borderRadius: "16px",
+              boxShadow: "0 4px 12px rgba(0, 0, 0, 0.15)"
             }}
             cover={
               <img
                 alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                src="https://wallpaperaccess.com/full/4382314.jpg"
+                style={{
+                  height: "200px",
+                  objectFit: "cover",
+                  borderRadius: "16px 16px 0 0"
+                }}
               />
             }
-            actions={[
-              <Link to="/YourBlog">
-                <div>YourBlog</div>
-              </Link>,
-              <Link to="/YourCom">
-                <div>YourCom</div>
-              </Link>,
-            ]}
           >
-            <Meta
-              title="YourName" 
-              description="Pongthanat_n@cmu.ac.th"
+            <Avatar size={64} src="https://wallpaperaccess.com/full/4382314.jpg" style={{ position: "absolute", top: "120px", left: "50%", transform: "translate(-50%, -50%)" }} />
+            <Divider />
+            <Card.Meta
+              title={<Title level={3}>YourName</Title>}
+              description={<Text>Pongthanat_n@cmu.ac.th</Text>}
             />
-          </Card>
-        </Col>
-
-        <Col span={8} align="middle">
-          <Card title="Card Title" style={{ display: 'flex', flexDirection: 'column' }}>
-            <Card.Grid style={{ ...gridStyle, flexDirection: 'row-reverse' }}>Content</Card.Grid>
-            <Card.Grid style={{ ...gridStyle, flexDirection: 'row-reverse' }}>Content</Card.Grid>
-            <Card.Grid style={{ ...gridStyle, flexDirection: 'row-reverse' }}>Content</Card.Grid>
-            <Card.Grid style={{ ...gridStyle, flexDirection: 'row-reverse' }}>Content</Card.Grid>
-            <Card.Grid style={{ ...gridStyle, flexDirection: 'row-reverse' }}>Content</Card.Grid>
-            <Card.Grid style={{ ...gridStyle, flexDirection: 'row-reverse' }}>Content</Card.Grid>
-            <Card.Grid style={{ ...gridStyle, flexDirection: 'row-reverse' }}>Content</Card.Grid>
+            <Divider />
+            <Row justify="center">
+              <Col span={12}>
+                <Button className="button-css" block size="large" href="/YourBlog">Your Blog</Button>
+              </Col>
+              <Col span={12}>
+                <Button className="button-css" block size="large" href="/YourCom">Your Community</Button>
+              </Col>
+            </Row>
           </Card>
         </Col>
       </Row>
-      
     </div>
   );
 };

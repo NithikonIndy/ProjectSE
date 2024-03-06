a3f75c174b5d616573975c694891238fa1cd2775
NithikonIndy <nithikon4410@hotmail.com>
2024-03-06 16:37:23 +0700

    555

--- Backend/server.js
+++ Backend/server.js
@@ -20,12 +20,11 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: ['https://project-se-gules.vercel.app','https://backend-b1ep.onrender.com'],
-  methods: ['GET', 'POST, PUT', 'DELETE', 'PATCH'],
+  origin: 'https://project-se-gules.vercel.app',
+  methods: 'GET, POST, PUT, DELETE, PATCH',
   credentials: true,
-  // exposedHeaders: 'set-cookie',
-  // allowedHeaders: ['Content-Type', 'Authorization', 'set-cookie'],
 }));
+
 
 app.use(cookieParser());
 app.use(express.json());

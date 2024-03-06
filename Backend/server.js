89ce228fc5ea73d731c87d0cd617e4bb0d1a4935
NithikonIndy <nithikon4410@hotmail.com>
2024-03-07 01:25:01 +0700

    คือไรอ่ะ

--- Backend/server.js
+++ Backend/server.js
@@ -30,6 +30,8 @@ app.enable('trust proxy')
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.disable('x-powered-by');
 
 app.use(
